5d51b3ef0391a2dbe3e0660e8114242ce6080175
Zeovetar <zeovetar@gmail.com>
2022-04-04 20:12:57 +0300

    1st step

---
+++ HW1/ValuesHolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ValuesHolder
+{
+    public List<string> Values { get; set; }
+	public ValuesHolder()
+	{
+	}
+
+    internal void Add(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal object Get()
+    {
+        throw new NotImplementedException();
+    }
+}
