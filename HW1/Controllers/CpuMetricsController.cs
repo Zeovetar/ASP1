0916a1fa3b54771c021317631890db65f825c691
Zeovetar <zeovetar@gmail.com>
2022-04-11 20:09:48 +0300

    step

--- HW1/Controllers/CpuMetricsController.cs
+++ HW1/Controllers/CpuMetricsController.cs
@@ -1,9 +1,6 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace MetricsManager.Controllers
 {
@@ -11,16 +8,27 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
+
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId,
         [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation("Привет! Это наше первое сообщение в лог");
             return Ok();
         }
         [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan
         fromTime, [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation("Привет! Это наше второе сообщение в лог");
             return Ok();
         }
     }
--- HW1/Startup.cs
+++ HW1/Startup.cs
@@ -25,6 +25,8 @@ namespace MetricsManager.Controllers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Services.AddSingleton<IValuesHolder, DictonaryValuesHolder>();
+            //Services.AddSingleton<IValuesHolder, ListValuesHolder>();
             services.AddSingleton<AgentsHolder>();
             services.AddControllers();
         }
